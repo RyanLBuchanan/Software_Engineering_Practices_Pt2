2fbd8277f7d8dff9e440f05de0b36c3ea6068365
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-11-09 10:53:23 -0700

    chore: Tested code

--- example.py
+++ example.py
@@ -1,2 +1,11 @@
 from shirt import Shirt
 
+shirt_one = Shirt('orange', 'M', 'short sleeve', 25)
+shirt_two = Shirt('red', 'S', 'short sleeve', 15)
+shirt_three = Shirt('purple', 'XL', 'short sleeve', 10)
+
+print(shirt_one.price)
+print(shirt_two.color)
+
+shirt_three.change_price(45)
+print(shirt_three.price)
